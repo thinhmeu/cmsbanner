865212ff2fde5d2117edccd059bbb427972b35eb
thinhmeu <thinhmeu@yahoo.com>
2022-07-05 10:07:04 +0700

    done api banner

--- app/Http/Controllers/api/BannerController.php
+++ app/Http/Controllers/api/BannerController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\ApiController;
+use http\Env\Request;
+use http\Env\Response;
 use Illuminate\Support\Facades\DB;
 
 class BannerController extends ApiController
 {
     public function index($siteName){
-        $data = DB::table('banner')->join('banner_site', 'id_website', '=', 'banner_site.id')
-            ->select('banner.link, banner.target, banner.rel, banner.image, banner.alt, banner.width, banner.height, banner_site.slug')
-            ->where('banner_site.slug', '=', $siteName)
-            ->get();
-        dd($data);
+        $data = DB::select("SELECT link, target, rel, image, alt, width, height, banner.start_date, banner.`end_date`, (SELECT banner_site.`slug` FROM banner_site WHERE `type` = 'position' AND id = id_position) AS vitri
+FROM banner JOIN banner_site ON id_website = banner_site.id 
+WHERE banner_site.`slug` = '$siteName' AND `status` = 1 AND NOW() BETWEEN IFNULL(start_date,'1900-01-01') AND IFNULL(end_date,NOW())");
+        $tmp = [];
+        foreach ($data as $item){
+            $tmp[$item->vitri][] = $item;
+        }
+
+        return response($tmp)->withHeaders([
+            'Content-Type' => 'application/json',
+            'X-Header-One' => 'Header Value',
+            'X-Header-Two' => 'Header Value',
+        ]);
+    /*echo json_encode($tmp);*/
     }
 }

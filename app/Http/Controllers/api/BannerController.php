20275da010e53850e789e016e4d5f57c6a098263
khanh <nd.khanh251289@gmail.com>
2022-07-28 20:12:19 +0700

    update order by

--- app/Http/Controllers/api/BannerController.php
+++ app/Http/Controllers/api/BannerController.php
@@ -11,8 +11,8 @@ class BannerController extends ApiController
 {
     public function index($siteName){
         $data = DB::select("SELECT link, target, rel, image, alt, width, height, (SELECT banner_site.`slug` FROM banner_site WHERE `type` = 'position' AND id = id_position) AS vitri
-FROM banner JOIN banner_site ON id_website = banner_site.id 
-WHERE banner_site.`slug` = '$siteName' AND `status` = 1 AND NOW() BETWEEN IFNULL(start_date,'1900-01-01') AND IFNULL(end_date,NOW())");
+FROM banner JOIN banner_site ON id_website = banner_site.id
+WHERE banner_site.`slug` = '$siteName' AND `status` = 1 AND NOW() BETWEEN IFNULL(start_date,'1900-01-01') AND IFNULL(end_date,NOW()) ORDER BY banner.`order`");
         $tmp = [];
         foreach ($data as $item){
             $tmp[$item->vitri][] = $item;
